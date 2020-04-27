c621c8155f916e966bdd4bdce2a95632768a4b69
rh46 <742754+rh46@users.noreply.github.com>
2020-04-27 06:06:55 -0400

    Use awk to parse csv

--- install-software.sh
+++ install-software.sh
@@ -24,20 +24,32 @@ while (( "$#" )); do
 done
 
 # creates array from profile files
-function get_profile_values {
-    local array_name=$(echo $1 | tr '[:upper:]' '[:lower:]') # make array name lower case
-    local location=$(echo $1 | tr '[:lower:]' '[:upper:]') # make location upper case
-
-    # put values from profiles/default into global array
-    declare -a -g ${array_name}=( $(sed -n "/$location/{n;p;}" profiles/default) )
-
-    case $PROFILE in
-        # sed -n "/$location/{n;p;}" looks for $1 and returns the line below
-        # example, $1=formulas so return line below FORMULAS in profiles/home where PROFILE=home
-        home) ${array_name}+=( $(sed -n "/$location/{n;p;}" profiles/home) ) ;;
-        work) ${array_name}+=( $(sed -n "/$location/{n;p;}" profiles/work) ) ;;
+function get_install_list {
+    local list_name=$(echo $1 | tr '[:upper:]' '[:lower:]') # make input lower case
+    
+    case $list_name in
+        # set type based on input
+        formulas) local type="formula" ;;
+        casks) local type="cask" ;;
+        mas_apps) local type="mas" ;;
+        pips) local type="pip" ;;
+        gems) local type="gem" ;;
+        vscode_exts) local type="vscode" ;;
         *) ;;
     esac
+
+    # put default apps from apps.csv into array
+    local apps=( $(awk -F ',' -v type=$type '{ if ($2 == type && $3 == "default") print $1 }' apps.csv) )
+
+    case $PROFILE in
+        # get list of apps for selected profile
+        home) apps+=( $(awk -F ',' -v type=$type '{ if ($2 == type && $3 == "home") print $1 }' apps.csv) ) ;;
+        work) apps+=( $(awk -F ',' -v type=$type '{ if ($2 == type && $3 == "work") print $1 }' apps.csv) ) ;;
+        *) ;;
+    esac
+
+    # print list
+    echo ${apps[@]}
 }
 
 function main {
@@ -64,46 +76,40 @@ function main {
     fi
 
     ## install homebrew formulas
-    get_profile_values formulas
-    for f in "${formulas[@]}"; do
+    for f in $(get_install_list formulas); do
         #brew install "$f"
         echo "brew install $f" # for testing
     done
 
     ## install homebrew casks
-    get_profile_values casks
-    for c in "${casks[@]}"; do
+    for c in $(get_install_list casks); do
         #brew cask install "$c"
         echo "brew cask install "$c"" # for testing
     done
 
     ## install mac apple store apps
-    get_profile_values mas_apps
-    for app in "${mas_apps[@]}"; do
-        #mas install "$app"
-        echo "mas install "$app"" # for testing
+    for a in $(get_install_list mas_apps); do
+        #mas install "$a"
+        echo "mas install "$a"" # for testing
     done
 
     ## configure python and install packages
     latest=$(pyenv install --list | grep " 3\.*" | grep -v dev | tail -n1 | awk '{$1=$1;print}') # set latest to stable version
     #pyenv install $latest
     #pyenv global 
-    get_profile_values pips
-    for p in "${pips[@]}"; do
+    for p in $(get_install_list pips); do
         #pip install "$p"
         echo "pip install "$p"" # for testing
     done
 
     ## install gems
-    get_profile_values gems
-    for g in "${gem[@]}"; do
+    for g in $(get_install_list gems); do
         #gem install "$g"
         echo "gem install "$g"" # for testing
     done
 
     ## install vscode extentions
-    get_profile_values vscode_exts
-    for e in "${vscode_exts[@]}"; do
+    for e in $(get_install_list vscode_exts); do
         #code --install-extension "$e"
         echo "code --install-extension "$e"" # for testing
     done
